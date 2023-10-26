a078606bb35c312d5e2a10c6f076a0245879ee2e
dewa0089 <newdewa03@gmail.com>
2023-10-26 13:40:38 +0700

    push

--- latihan_uts/lib/main.dart
+++ latihan_uts/lib/main.dart
@@ -12,26 +12,36 @@ class MyApp extends StatelessWidget {
             title: Text('Aplikasi Sepatu'),
             backgroundColor: Colors.deepOrange,
           ),
-          body: Column(children: <Widget>[
-            Container(
-                //Judul
-                margin: const EdgeInsets.only(top: 16.0),
-                child: Text('Nike Air Jordan',
-                    textAlign: TextAlign.left,
-                    style: const TextStyle(
-                        fontSize: 30.0, fontFamily: 'Staatliches'
-                        //fontWeight: FontWeight.bold,
-                        ))),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          body: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Image.network(
-                    'https://www.petanikode.com/img/flutter/flutter-sqr.png'),
-                Image.network(
-                    'https://www.petanikode.com/img/flutter/flutter-sqr.png')
-              ],
-            )
-          ])),
+                Container(
+                    //Judul
+                    margin: const EdgeInsets.only(top: 16.0, left: 16.0),
+                    child: Text('Nike Air Jordan X',
+                        textAlign: TextAlign.left,
+                        style: const TextStyle(
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                        ))),
+                Container(
+                    //Judul
+                    margin: const EdgeInsets.only(top: 16.0, left: 16.0),
+                    child: Text('4.9',
+                        textAlign: TextAlign.left,
+                        style: const TextStyle(
+                          fontSize: 19.0,
+                        ))),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Image.network(
+                        'https://www.petanikode.com/img/flutter/flutter-sqr.png'),
+                    Image.network(
+                        'https://www.petanikode.com/img/flutter/flutter-sqr.png')
+                  ],
+                )
+              ])),
     );
   }
 }
