067417a366bacfb094aa372069df9f21ffd87f58
dewa0089 <newdewa03@gmail.com>
2023-10-29 16:22:41 +0700

    push

--- latihan/android/app/src/main/kotlin/com/example/aplikasi_wisata_lembang/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.aplikasi_wisata_lembang
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- latihan_uts/lib/main.dart
+++ latihan_uts/lib/main.dart
@@ -8,40 +8,95 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'First App',
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Aplikasi Sepatu'),
-            backgroundColor: Colors.deepOrange,
-          ),
           body: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Container(
-                    //Judul
-                    margin: const EdgeInsets.only(top: 16.0, left: 16.0),
-                    child: Text('Nike Air Jordan X',
-                        textAlign: TextAlign.left,
-                        style: const TextStyle(
-                          fontSize: 30.0,
-                          fontWeight: FontWeight.bold,
-                        ))),
-                Container(
-                    //Judul
-                    margin: const EdgeInsets.only(top: 16.0, left: 16.0),
-                    child: Text('4.9',
-                        textAlign: TextAlign.left,
-                        style: const TextStyle(
-                          fontSize: 19.0,
-                        ))),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Image.network(
-                        'https://www.petanikode.com/img/flutter/flutter-sqr.png'),
-                    Image.network(
-                        'https://www.petanikode.com/img/flutter/flutter-sqr.png')
-                  ],
+            Container(
+                //Judul
+                margin: const EdgeInsets.only(top: 16.0, left: 16.0),
+                child: Text('Nike Air Jordan X',
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ))),
+            Container(
+              margin: const EdgeInsets.only(top: 5.0, left: 16.0),
+              child: Row(children: <Widget>[
+                Row(children: <Widget>[
+                  const Icon(Icons.star, color: Colors.yellow),
+                  const SizedBox(height: 8.0),
+                  Text(' 4,9 '),
+                  Text('(130 Reviews)', style: TextStyle(color: Colors.grey))
+                ]),
+              ]),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Image.asset(
+                  'images/sepatu.jpg',
+                  fit: BoxFit.cover,
                 )
-              ])),
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 5.0, left: 16.0),
+              child: Row(children: <Widget>[
+                Row(children: <Widget>[
+                  const SizedBox(height: 8.0),
+                  Text(
+                    ' Color      ',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  const Icon(Icons.circle, color: Colors.black),
+                  const Icon(Icons.circle, color: Colors.orange),
+                  const Icon(Icons.circle, color: Colors.red),
+                ]),
+              ]),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 5.0, left: 16.0),
+              child: Row(children: <Widget>[
+                Row(children: <Widget>[
+                  const SizedBox(height: 8.0),
+                  Text(
+                    ' Description',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )
+                ]),
+              ]),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 5.0, left: 16.0),
+              child: Row(children: <Widget>[
+                Row(children: <Widget>[
+                  const SizedBox(height: 8.0),
+                  Text(' Sepatu ini jual lohh')
+                ]),
+              ]),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 5.0, left: 16.0),
+              margin:EdgeInsets.symmetric(vertical: 4)
+              child: Column(children: <Widget>[
+                Column(children: <Widget>[
+                  const SizedBox(height: 8.0),
+                  Text(
+                    ' Size',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ]),
+                Column(children: <Widget>[
+                  const SizedBox(height: 8.0),
+                  Text(
+                    ' Size',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ]),
+              ]),
+            ),
+          ])),
     );
   }
 }
